8a2b7abc0c7a373c9dc5ab82e4e2c40dba36e43c
arjunraghurama <arjunr0019@gmail.com>
2021-01-24 00:03:26 +0530

    Update 015.singletonDesignPattern.cpp

--- 015.singletonDesignPattern.cpp
+++ 015.singletonDesignPattern.cpp
@@ -7,7 +7,7 @@ class Singleton
     static Singleton* INSTANCE;
     string s1="Initial";
     public:
-        Singleton* getInstance()
+        static Singleton* getInstance()
         {
             if(!INSTANCE)
             {
@@ -15,11 +15,13 @@ class Singleton
             }
             return INSTANCE;
         }
+
         bool set(string str)
         {
           s1 = str;
           return true;
         }
+
         string get()
         {
           return s1;
@@ -28,21 +30,23 @@ class Singleton
         Singleton(){}
 };
 
-Singleton *Singleton::INSTANCE = 0;
+Singleton *Singleton::INSTANCE;
 
 int main() {
 
-  Singleton *firstObject = firstObject->getInstance();
+  Singleton *firstObject = Singleton::getInstance();
   cout<< firstObject->get()<<endl;
 
   firstObject->set("First value");
 
-  Singleton *secondObject = secondObject->getInstance();
+  Singleton *secondObject = Singleton::getInstance();
   cout<< secondObject->get()<<endl;
 
   if(firstObject == secondObject)
   {
     cout<< "Both objects are same "<<endl;
   }
+  
+  return 0;
 
 }
